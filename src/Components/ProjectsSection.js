c3b6a12bd4763d31116653052db696a8de3d3483
Arjun Cosare <arjun.cosare@outlook.com>
2020-07-23 17:39:39 -0600

    View all project button update

--- src/Components/ProjectsSection.js
+++ src/Components/ProjectsSection.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import './ProjectsSection.css';
+import arrow from '../images/play_arrow-24px.png';
 
 class ProjectsSection extends Component{
     render(){
         return(
             <div className="project-section-container">
                 <header>
-                    <h1>Our Projects</h1>
-                    <a href="#">
-                        <div className="view-all-project-button">
-                            <h3>
+                    <h1>Our Projects</h1>                   
+                    <a href="#" className="button-container">
+                        <div className="view-all-project-button-left">
+                            <h4>
                                 View All Projects
-                            </h3>
+                            </h4>
                         </div>
-                    </a>
+                        <div className="view-all-project-button-right">                    
+                            <img src={arrow} alt=""></img>     
+                        </div>
+                    </a>       
                 </header>
                 <div className="project-info-contents">
 
