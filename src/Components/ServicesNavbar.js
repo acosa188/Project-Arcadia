a53d744a92043f43a1dbd4fc0bc5628aa1e1ee83
Arjun Cosare <arjun.cosare@outlook.com>
2020-07-23 16:41:48 -0600

    ServiceNavbar update

--- src/Components/ServicesNavbar.js
+++ src/Components/ServicesNavbar.js
@@ -6,11 +6,39 @@ class ServicesNavbar extends Component{
     render(){
         return(
             <div className="services-navbar-container">
-                <nav className="services-navbar">
-                    <a href="#" className="services-item">Branding</a>
-                    <a href="#" className="services-item">UI/UX</a>
-                    <a href="#" className="services-item">Animation</a>
-                    <a href="#" className="services-item">Advertising</a>
+                <nav className="services-navbar">  
+                    <a href="#" className="services-item">
+                        <div class="nav-item">
+                            <h5>01</h5>
+                            <p>
+                                Branding
+                            </p>
+                        </div>
+                    </a>
+                    <a href="#" className="services-item">
+                        <div class="nav-item">
+                            <h5>02</h5>
+                            <p>
+                                UI/UX
+                            </p>
+                        </div>
+                    </a>
+                    <a href="#" className="services-item">
+                        <div class="nav-item">
+                            <h5>03</h5>
+                            <p>
+                                Animation
+                            </p>
+                        </div>
+                    </a>
+                    <a href="#" className="services-item">
+                        <div class="nav-item">
+                            <h5>04</h5>
+                            <p>
+                                Advertising
+                            </p>
+                        </div>
+                    </a>
                 </nav>
             </div>
         );
