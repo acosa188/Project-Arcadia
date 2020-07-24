cc87b1c90d1063147a6473e3cba60200a2837f5c
Arjun Cosare <arjun.cosare@outlook.com>
2020-07-23 19:23:46 -0600

    Scuffed logo kekw

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import HeaderNavbar from './HeaderNavbar';
 import './Footer.css';
+import logo from '../images/logo.png';
 
 class Footer extends Component{
     render(){
         return(
             <div className="footer-container">
                 <div className="logoClass">
-
+                    <img src={logo} alt="arcadia-logo"></img>
                 </div>
                 <HeaderNavbar />
                 <div className="logoClass">
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import HeaderNavbar from './HeaderNavbar';
 import './Header.css';
+import logo from '../images/logo.png';
 
 class Header extends Component{
     render(){
         return(
             <div className="headerClass">
-                <div className="logoClass">
-
-                </div>
+                <a href="#" className="logoClass">
+                    <img src={logo} alt="arcadia-logo"></img>
+                </a>
                 <nav className="navigation" >
                     <HeaderNavbar />
                 </nav>
