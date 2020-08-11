f4de6c95801424b995559a0482667f59e9f67a49
Arjun Cosare <arjun.cosare@outlook.com>
2020-08-11 15:15:56 -0600

    react-bootstrap integration

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import React from 'react';
-import Header from './Components/Header';
+import Navbar from './Components/Navbar';
 import ServicesNavbar from './Components/ServicesNavbar';
 import ServicesContent from './Components/SevicesContent';
 import ServiceInfoSection from './Components/ServiceInfoSection';
 import ProjectsSection from './Components/ProjectsSection';
 import TestimonialSection from './Components/TestimonialSection';
 import Footer from './Components/Footer';
+import { Container, Jumbotron } from 'react-bootstrap';
 import './css-reset.css';
 import './App.css';
 
 function App() {
   return (
     <div className="Appwrapper">
-      <div className="App">
-            <header>
-              <Header />
-            </header>
-            <main>
+      <Container>
+        <div className="App">
+          <header>
+            <Navbar />
+          </header>
+          <main>
+            <Jumbotron fluid>
               <h2 className="company-title">ARCADIA</h2>
+            </Jumbotron>
+            <Jumbotron>
               <h1 className="company-motto">We evolve ideas & products into brands</h1>
-              <ServicesNavbar />
-              <ServicesContent />
-              <ServiceInfoSection />
-              <ProjectsSection />
-              <TestimonialSection />
-            </main> 
-          </div>
+            </Jumbotron>
+            <ServicesNavbar />
+            <ServicesContent />
+            <ServiceInfoSection />
+            <ProjectsSection />
+            <TestimonialSection />
+          </main>
+        </div>
+      </Container>
 
-          <footer className="footerClass">
-            <Footer />
-          </footer>
+      
+      <footer className="footerClass">
+        <Footer />
+      </footer>
+      
+
     </div>
-    
+
   );
 }
 
